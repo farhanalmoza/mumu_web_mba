2022a179de81aa312f2afff43ea8bfcebe3dc47a
Farkhan <45299504+farhanalmoza@users.noreply.github.com>
2025-04-26 22:15:41 +0000

    add title

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.title("Market Basket Analysis")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Total Transaction")
+
+with col2:
+    st.header("Total Data")
+
+with col3:
+    st.header("Total Item")
